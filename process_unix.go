596e6c93011ecef07f3093d6f00966a406094c14
mductran <ductran032118@gmail.com>
2023-11-18 22:14:33 +0700

    implement: search and list processes on linux

--- process_unix.go
+++ process_unix.go
@@ -55,3 +55,41 @@ func list() ([]Process, error) {
 
 	return processes, nil
 }
+
+func searchByName(processes *[]Process, target string) ([]Process, error) {
+	var out []Process
+	for _, process := range *processes {
+		if process.name == target {
+			out = append(out, process)
+		}
+	}
+
+	return out, nil
+}
+
+func searchByPid(processes *[]Process, target uint32) ([]Process, error) {
+	var out []Process
+	for _, process := range *processes {
+		if process.pid == target {
+			out = append(out, process)
+		}
+	}
+
+	return out, nil
+}
+
+func start(path string) (uint32, error) {
+
+}
+
+func stop(pid uint32) (bool, error) {
+
+}
+
+func suspend(pid uint32) (bool, error) {
+
+}
+
+func resume(pid uint32) (bool, error) {
+
+}
